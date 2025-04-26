9b085c1a22ec45f39df91cf9ed886e03161271fa
Tom Palmer <remlapmot@hotmail.com>
2025-04-26 20:26:10 +0100

    Amend log filenames

--- space in-filepath/setup-linux.sh
+++ space in-filepath/setup-linux.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=/usr/local/stata18/utilities
 uv venv --python 3.13
 source .venv/bin/activate
-python < example.py > example-macos.log
+python < example.py > example-linux.log
 deactivate
