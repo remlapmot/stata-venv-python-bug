63377f865e66533718597c5e75c6d56ed1530702
Tom Palmer <remlapmot@hotmail.com>
2025-04-26 17:25:54 +0100

    Update setup-macos.sh

--- no-spaces-in-filepath/setup-macos.sh
+++ no-spaces-in-filepath/setup-macos.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=/Applications/Stata/utilities
 uv venv --python 3.13
 source .venv/bin/activate
-python < example.py > example.log
+python < example.py > example-macos.log
 deactivate
