0d5f0de754cd11f349e2b4cdce14af5508738c14
Tom Palmer <remlapmot@hotmail.com>
2025-04-27 18:37:32 +0100

    Obtain utf8 log on Windows

--- space in-filepath/setup-windows.sh
+++ space in-filepath/setup-windows.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH="C:/Program Files/Stata18/utilities"
 uv venv --python 3.13
 source .venv/Scripts/activate
-python < example.py > example-windows.log
+python -X utf8 < example.py > example-windows.log
 deactivate
