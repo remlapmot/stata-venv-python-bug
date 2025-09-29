770fb8140fdb712693ec9c3e3deaf3f513ebebf8
Tom Palmer <remlapmot@hotmail.com>
2025-09-29 17:49:53 +0100

    Update macOS example in StataNow 19.5

--- space in-filepath/setup-macos.sh
+++ space in-filepath/setup-macos.sh
@@ -1,4 +1,4 @@
-export PYTHONPATH=/Applications/Stata/utilities
+export PYTHONPATH=/Applications/StataNow/utilities
 uv venv --python 3.13
 source .venv/bin/activate
 python < example.py > example-macos.log
